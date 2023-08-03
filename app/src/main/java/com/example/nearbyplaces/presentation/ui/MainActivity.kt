6f65dc65bb7e72cb25b5580ffadbf649b3cee050
Azro08 <azro.skib08@gmail.com>
2023-08-03 23:54:43 +0300

    initial commit

---
+++ app/src/main/java/com/example/nearbyplaces/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nearbyplaces.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nearbyplaces.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
