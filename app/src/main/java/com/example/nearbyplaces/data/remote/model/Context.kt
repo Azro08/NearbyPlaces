29d93726d23372ba2281a055f65d10e0665af168
Azro08 <69281457+Azro08@users.noreply.github.com>
2023-08-04 03:03:18 +0300

    Revert "Features/add auth token"

--- app/src/main/java/com/example/nearbyplaces/data/remote/model/Context.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nearbyplaces.data.remote.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Context(
-    @SerializedName("geo_bounds")
-    val geoBounds: GeoBounds
-)
--- app/src/main/java/com/example/nearbyplaces/data/remote/model/PlaceResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nearbyplaces.data.remote.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class PlaceResponse(
-    @SerializedName("results")
-    val results: List<Results>
-)
